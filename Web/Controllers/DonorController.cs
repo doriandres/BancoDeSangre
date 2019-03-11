b7f57146573361ce63dccfff1768f8663894b93a
Cristhian Martínez <cmartinezh184@ulacit.ed.cr>
2019-03-10 18:17:08 -0600

    Se cambió el nombre del método Save a SaveDonor. Se agregó valdiación de Id para el método de SaveDonor. Se agregó el método SaveMedicalCenter con las validaciones respectivas que sirve para agregar centros médicos como Manager.

--- Web/Controllers/DonorController.cs
+++ Web/Controllers/DonorController.cs
@@ -36,7 +36,7 @@ namespace BancoDeSangre.Controllers
         /// <param name="donor">Donor information to be saved</param>
         /// <returns>JSON with result of the operation</returns>
         [HttpPost]
-        public ActionResult Save(Donor donor)
+        public ActionResult SaveDonor(Donor donor)
         {
             if (!Session.IsSignedIn())
             {
@@ -96,9 +96,63 @@ namespace BancoDeSangre.Controllers
                     valid = false;
                 }
 
+                if (donor.Id < 100000000 & donor.Id > 999999999)
+                {
+                    cause = "Debe ingresar un n\u00famero de identificaci\00f3n v\u00e1lido";
+                    valid = false;
+                }
+
                 if (!valid) return Json(new { saved = false, cause });
 
                 db.Donors.Add(donor);
+                var count = db.SaveChanges();
+                return Json(new { saved = count > 0 });
+            }
+        }
+
+        /// <summary>
+        /// Creates a medical center
+        /// </summary>
+        /// <param name="medicalCenter">Medical center information to be saved</param>
+        /// <returns>JSON with result of the operation</returns>
+        [HttpPost]
+        public ActionResult SaveMedicalCenter(MedicalCenter medicalCenter)
+        {
+            if (!Session.IsSignedIn())
+            {
+                return Json(new { saved = false });
+            }
+
+            using (var db = new DataBaseService())
+            {
+                var valid = true;
+                var cause = "";
+
+                if (medicalCenter.Name.Trim().Length == 0)
+                {
+                    cause = "Debe ingresar un nombre";
+                    valid = false;
+                }
+
+                if (medicalCenter.Email.Trim().Length == 0)
+                {
+                    cause = "Debe ingresar un correo";
+                    valid = false;
+                }
+
+                if (medicalCenter.PhoneNumber < 20000000)
+                {
+                    cause = "Debe ingresar un n\u00famero de tel\u00E9fono v\u00E1lido";
+                    valid = false;
+                }
+
+                if (medicalCenter.Place.Trim().Length == 0)
+                {
+                    cause = "Debe ingresar una localizaci\u00e9n";
+                    valid = false;
+                }
+
+                db.MedicalCenters.Add(medicalCenter);
                 var count = db.SaveChanges();
                 return Json(new { saved = count > 0 });
             }
