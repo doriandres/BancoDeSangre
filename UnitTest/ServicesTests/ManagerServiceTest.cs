a59fee4673c2ea2c3bf89d094d76ecb5ddf3b16b
abolanosr <47220675+abolanosr@users.noreply.github.com>
2019-03-18 19:19:19 -0600

    tester
    tester

--- UnitTest/ServicesTests/ManagerServiceTest.cs
+++ UnitTest/ServicesTests/ManagerServiceTest.cs
@@ -62,5 +62,53 @@ namespace UnitTest.ServicesTests
                 Assert.IsTrue(result, "Manager was not removed");
             }
         }
+        [TestMethod]
+        public void Donor_add_Tests()
+        {
+
+            //Arrange 
+            var donationservice = new DonationDBService();
+            Donation donation = new Donation();
+
+
+            // Act
+            var resultado = donationservice.Equals(donation);
+
+
+            // Assert
+            Assert.IsFalse(resultado);
+
+        }
+
+        [TestMethod]
+        public void Manager_Service_Tests()
+        {
+            //Arrange
+            var managerservice = new ManagerDBService();
+            Manager manager = new Manager();
+
+            // Act
+
+            var resultado = manager.GetType();
+
+            // Assert
+            Assert.IsFalse();
+        }
+        [TestMethod]
+        public void Medical_Center_Test()
+        {
+            //Arrange
+            var medicalcenter = new MedicalCenterDBService();
+            MedicalCenter save = new MedicalCenter();
+
+            //Act
+
+            var resultado = MedicalCenter.Empty();
+
+            //Asset
+            Assert.Fail();
+        }
+
+
     }
 }
