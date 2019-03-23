a2e645c37043bf054b10307264c68ff9bc0492e4
Dorian Andrés <dorian.andres.cortes@gmail.com>
2019-03-23 16:17:11 -0600

    Remodelación de la pantalla de crear campaña

--- Web/Public/JavaScript/views/campaign/create.js
+++ Web/Public/JavaScript/views/campaign/create.js
@@ -1,9 +1,19 @@
-﻿import ajax from './../../components/ajax.js';
+﻿import ajax from "./../../components/ajax.js";
+import modal from "./../../components/modal.js";
+import datepicker from "./../../components/datepicker.js";
+import timepicker from "./../../components/timepicker.js";
+
 (() => {
+    datepicker();
+    timepicker();
+
     var campaignForm = document.querySelector("form");
     var request = null;
+    var submitBtn = document.getElementById("submit-btn");
+
     function signIn(event) {
         event.preventDefault();
+        submitBtn.classList.add("disabled");
         if (request && request.readyState < 4) return;
         request = ajax({
             data: campaignForm,
@@ -11,10 +21,9 @@
                 if (response.saved) {
                     campaignForm.reset();
                     window.location = "/";
-                } else if (response.cause) {
-                    alert(response.cause);
-                } else {
-                    alert("No se pudo crear la campaña intentelo más tarde");
+                }  else {
+                    modal("Ocurrio un error", response.cause || "No se puede procesar su solicitud en este momento");                    
+                    submitBtn.classList.remove("disabled");s
                 }
             }
         });
