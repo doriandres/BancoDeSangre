779c1f50e582916428b61a4442dcbb52d417d1b1
Dorian Andrés <dorian.andres.cortes@gmail.com>
2019-03-23 12:52:43 -0600

    Se incluyo el modal y se mejoró el inicio de sesión

--- Web/Public/JavaScript/views/manager/sign-in.js
+++ Web/Public/JavaScript/views/manager/sign-in.js
@@ -1,18 +1,22 @@
 ﻿import ajax from './../../components/ajax.js';
+import modal from './../../components/modal.js';
 (() => {
     var signInForm = document.querySelector("form");
+    var submitBtn = document.getElementById("submit-btn");
     var request = null;
     function signIn(event) {
         event.preventDefault();
+        submitBtn.classList.add("disabled");
         if (request && request.readyState < 4) return; // Hay un request en proceso o el formulario es invalido        
         request = ajax({
             data: signInForm,
-            onResponse: response => {
+            onResponse: response => {                
                 if (response.valid) {
                     signInForm.reset();
                     window.location = "/";
                 } else {
-                    alert(response.cause);
+                    modal("Ocurrió un error", response.cause || "En este momento no podemos procesar su solicitud");
+                    submitBtn.classList.remove("disabled");
                 }
             }
         });        
