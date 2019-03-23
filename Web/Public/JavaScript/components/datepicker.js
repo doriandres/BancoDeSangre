968f73e520614be99282702df308344a92c1eccd
Dorian Andrés <dorian.andres.cortes@gmail.com>
2019-03-23 16:17:30 -0600

    Se crearon los modulos de inicialización del datepicker y del timepicker

---
+++ Web/Public/JavaScript/components/datepicker.js
@@ -0,0 +1,61 @@
+﻿export default function () {
+    var datePickers = document.querySelectorAll(".datepicker");
+    datePickers.forEach(dp => dp.setAttribute("type", "text"));
+    if (datePickers.length) {
+        M.Datepicker.init(datePickers, {
+            format: "mm-dd-yyyy",
+            i18n: {
+                cancel: "Cancelar",
+                clear: "Limpiar",
+                done: "Listo",
+                months: [
+                    "Enero",
+                    "Febrero",
+                    "Marzo",
+                    "Abril",
+                    "Mayo",
+                    "Junio",
+                    "Julio",
+                    "Augosto",
+                    "Setiembre",
+                    "Octubre",
+                    "Noviembre",
+                    "Diciembre"
+                ],
+                monthsShort: [
+                    "Ene",
+                    "Feb",
+                    "Mar",
+                    "Abr",
+                    "May",
+                    "Jun",
+                    "Jul",
+                    "Ago",
+                    "Set",
+                    "Oct",
+                    "Nov",
+                    "Dic"
+                ],
+                weekdays: [
+                    "Domingo",
+                    "Lunes",
+                    "Martes",
+                    "Miércoles",
+                    "Jueves",
+                    "Viernes",
+                    "Sábado"
+                ],
+                weekdaysShort: [
+                    "Dom",
+                    "Lun",
+                    "Mar",
+                    "Mie",
+                    "Jue",
+                    "Vie",
+                    "Sab"
+                ],
+                weekdaysAbbrev: ["D", "L", "K", "M", "J", "V", "S"]
+            }
+        });
+    }    
+}
---
+++ Web/Public/JavaScript/components/timepicker.js
@@ -0,0 +1,13 @@
+﻿export default function () {
+    var timePickers = document.querySelectorAll(".timepicker");
+    timePickers.forEach(tp => tp.setAttribute("type", "text"));
+    if (timePickers.length) {
+        M.Timepicker.init(timePickers, {
+            i18n: {
+                cancel: "Cancelar",
+                clear: "Limpiar",
+                done: "Listo"
+            }
+        });
+    }    
+}
