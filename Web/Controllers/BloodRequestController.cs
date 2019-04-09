a65140e4a019d4b430a17168a684b5cfdd640f23
abolanosr <47220675+abolanosr@users.noreply.github.com>
2019-04-08 21:16:19 -0600

    controller

--- Web/Controllers/BloodRequestController.cs
+++ Web/Controllers/BloodRequestController.cs
@@ -1,9 +1,17 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
 namespace BancoDeSangre.Controllers
 {
     public class BloodRequestController : Controller
     {
-        
+        // GET: BloodRequest
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
---
+++ Web/Models/BancoDeSangreContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BancoDeSangre.Models
+{
+    public class BancoDeSangreContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public BancoDeSangreContext() : base("name=BancoDeSangreContext")
+        {
+        }
+
+		public System.Data.Entity.DbSet<BancoDeSangre.Models.Donation> Donations { get; set; }
+
+		public System.Data.Entity.DbSet<BancoDeSangre.Models.Donor> Donors { get; set; }
+	}
+}
