1d23e7809e1104d773c38cdc11432add1bf7e464
robq97 <39276231+robq97@users.noreply.github.com>
2019-04-11 13:27:20 -0600

    Arreglando algunos errores de compilación

--- Web/App_Start/WebApiConfig.cs
+++ Web/App_Start/WebApiConfig.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web.Http;
 
-namespace BancoDeSangre
-{
-    public static class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
-            config.MapHttpAttributeRoutes();
+//namespace BancoDeSangre
+//{
+//    public static class WebApiConfig
+//    {
+//        public static void Register(HttpConfiguration config)
+//        {
+//            config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
-    }
-}
+//            config.Routes.MapHttpRoute(
+//                name: "DefaultApi",
+//                routeTemplate: "api/{controller}/{id}",
+//                defaults: new { id = RouteParameter.Optional }
+//            );
+//        }
+//    }
+//}
--- Web/Controllers/DonationsController.cs
+++ Web/Controllers/DonationsController.cs
@@ -1,122 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
+﻿using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using BancoDeSangre.Models;
+using BancoDeSangre.Services.DonationService;
 
 namespace BancoDeSangre.Controllers
 {
     public class DonationsController : Controller
     {
-        private BancoDeSangreContext db = new BancoDeSangreContext();
+        private IDonationService donationService;
+
+        
+        public DonationsController(IDonationService donationService)
+        {
+            this.donationService = donationService;
+        }
 
         // GET: Donations
-        public ActionResult Index()
-        {
-            var donations = db.Donations.Include(d => d.Donor);
-            return View(donations.ToList());
-        }
+        //public ActionResult Index()
+        //{
+        //    var donations = db.Donations.Include(d => d.Donor);
+        //    return View(donations.ToList());
+        //}
 
-        // GET: Donations/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Donation donation = db.Donations.Find(id);
-            if (donation == null)
-            {
-                return HttpNotFound();
-            }
-            return View(donation);
-        }
+        //// GET: Donations/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Donation donation = db.Donations.Find(id);
+        //    if (donation == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(donation);
+        //}
 
-        // GET: Donations/Create
-        public ActionResult Create()
-        {
-            ViewBag.DonorId = new SelectList(db.Donors, "Id", "Email");
-            return View();
-        }
+        //// GET: Donations/Create
+        //public ActionResult Create()
+        //{
+        //    ViewBag.DonorId = new SelectList(db.Donors, "Id", "Email");
+        //    return View();
+        //}
 
-        // POST: Donations/Create
-        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
-        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,BloodType,Amount,Unit,Date,DonorId")] Donation donation)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Donations.Add(donation);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+        //// POST: Donations/Create
+        //// Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        //// más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "Id,BloodType,Amount,Unit,Date,DonorId")] Donation donation)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        //db.Donations.Add(donation);
+        //        //db.SaveChanges();
+        //        //return RedirectToAction("Index");
+        //    }
 
-            ViewBag.DonorId = new SelectList(db.Donors, "Id", "Email", donation.DonorId);
-            return View(donation);
-        }
+        //    ViewBag.DonorId = new SelectList(db.Donors, "Id", "Email", donation.DonorId);
+        //    return View(donation);
+        //}
 
-        // GET: Donations/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Donation donation = db.Donations.Find(id);
-            if (donation == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.DonorId = new SelectList(db.Donors, "Id", "Email", donation.DonorId);
-            return View(donation);
-        }
+        //// GET: Donations/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Donation donation = db.Donations.Find(id);
+        //    if (donation == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    ViewBag.DonorId = new SelectList(db.Donors, "Id", "Email", donation.DonorId);
+        //    return View(donation);
+        //}
 
-        // POST: Donations/Edit/5
-        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
-        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,BloodType,Amount,Unit,Date,DonorId")] Donation donation)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(donation).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.DonorId = new SelectList(db.Donors, "Id", "Email", donation.DonorId);
-            return View(donation);
-        }
+        //// POST: Donations/Edit/5
+        //// Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        //// más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "Id,BloodType,Amount,Unit,Date,DonorId")] Donation donation)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        //db.Entry(donation).State = EntityState.Modified;
+        //        //db.SaveChanges();
+        //        //return RedirectToAction("Index");
+        //    }
+        //    ViewBag.DonorId = new SelectList(db.Donors, "Id", "Email", donation.DonorId);
+        //    return View(donation);
+        //}
 
-        // GET: Donations/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Donation donation = db.Donations.Find(id);
-            if (donation == null)
-            {
-                return HttpNotFound();
-            }
-            return View(donation);
-        }
+        //// GET: Donations/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Donation donation = db.Donations.Find(id);
+        //    if (donation == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(donation);
+        //}
 
         // POST: Donations/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Donation donation = db.Donations.Find(id);
-            db.Donations.Remove(donation);
-            db.SaveChanges();
+            //Donation donation = db.Donations.Find(id);
+            //db.Donations.Remove(donation);
+            //db.SaveChanges();
             return RedirectToAction("Index");
         }
 
@@ -124,7 +126,7 @@ namespace BancoDeSangre.Controllers
         {
             if (disposing)
             {
-                db.Dispose();
+                //db.Dispose();
             }
             base.Dispose(disposing);
         }
