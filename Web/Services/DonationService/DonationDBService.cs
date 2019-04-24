79e0d66d425770607a7e4a9b9485bde9e5074844
Cristhian Martínez <cmartinezh184@ulacit.ed.cr>
2019-04-23 23:55:50 -0600

    Funcionalidad de donaciones completa

--- Web/Services/DonationService/DonationDBService.cs
+++ Web/Services/DonationService/DonationDBService.cs
@@ -7,58 +7,56 @@ using BancoDeSangre.Services.ManagerService;
 
 namespace BancoDeSangre.Services.DonationService
 {
-    public class DonationDBService : DBService, IDonationService
-    {
-        private IManagerService managerService;
+	public class DonationDBService : DBService, IDonationService
+	{
+		private IManagerService managerService;
 		public DonationDBService(IDataBaseService dbservice, IManagerService managerService) : base(dbservice) {
-            this.managerService = managerService;
-        }
+			this.managerService = managerService;
+		}
 
-        public bool CreateDonation(Donation donation)
-        {
-            dataBase.Donations.Add(donation);
-            var countChanges = dataBase.SaveChanges();
-            return true;
-        }
+		public bool CreateDonation(Donation donation)
+		{
+			dataBase.Donations.Add(donation);
+			var countChanges = dataBase.SaveChanges();
+			return true;
+		}
 
-        public Donation FindByID(int id)
-        {
-            return dataBase.Donations.FirstOrDefault(donation => donation.Id == id);
-        }
+		public Donation FindByID(int id)
+		{
+			return dataBase.Donations.FirstOrDefault(donation => donation.Id == id);
+		}
 
-        public bool IsValidDonation(Donation donation, out string cause)
-        {
-            var valid = true;
-            cause = "";
+		public bool IsValidDonation(Donation donation, out string cause)
+		{
+			var valid = true;
+			cause = "";
 
-            //Se puede agregar una validacion sobre el tipo de sangre O- para que lo guarde como donador especial
+			if (donation.Date > DateTime.Today)
+			{
+				cause = "No puede recibir donaciones en el futuro";
+				valid = false;
+			}
 
-            if (donation.Date > DateTime.Today)
-            {
-                cause = "No puede recibir donaciones en el futuro";
-                valid = false;
-            }
+			if (donation.Amount > 500)
+			{
+				cause = "No puede recibir donaciones mayores a medio litro (500 cc o 500 mL)";
+				valid = false;
+			}
 
-            if (donation.Amount > 500)
-            {
-                cause = "No puede recibir donaciones mayores a medio litro (500 cc o 500 mL)";
-                valid = false;
-            }
+			if (donation.DonorId == 0)
+			{
+				cause = "Donante inv\u00E1lido";
+				valid = false;
+			}
 
-            if (donation.DonorId == 0)
-            {
-                cause = "Donante inv\u00E1lido";
-                valid = false;
-            }
+			return valid;
+		}
 
-            return valid;
-        }
-
-        public List<Donation> FindAll()
-        {
-            var results = dataBase.Donations.ToList();
-            results.Reverse();
-            return results;
-        }
-    }
+		public List<Donation> FindAll()
+		{
+			var results = dataBase.Donations.ToList();
+			results.Reverse();
+			return results;
+		}
+	}
 }
